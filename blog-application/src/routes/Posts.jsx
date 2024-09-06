b893fe5b89010e68b375367356ea05c4640e6957
MatthewSawatz <msawatzky2@rrc.ca>
2024-09-06 12:54:15 -0500

    routing

---
+++ blog-application/src/routes/Posts.jsx
@@ -0,0 +1,14 @@
+import PostsList from "../components/PostsList";
+import { useState } from "react";
+
+function Posts() {
+  return (
+    <>
+      <main>
+        <PostsList />
+      </main>
+    </>
+  );
+}
+
+export default Posts;
